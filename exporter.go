6a918cb60f54521d7e994debc0ebdf839f3fefde
Apurva Chitnis <apuchitnis@hotmail.com>
2021-11-21 18:12:23 +0000

    print basic text

--- exporter.go
+++ exporter.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
-	fmt.Println("hello")
+	db, err := sql.Open("sqlite3", "../KoboReader.sqlite")
+	checkErr(err)
+
+	rows, err := db.Query("SELECT VolumeID, ContentID, Text FROM Bookmark WHERE VolumeID=\"11221b5b-04ef-4964-a8db-76396a0c9f77\" LIMIT 100")
+	checkErr(err)
+
+	var volumeID string
+	var contentID string
+	var text sql.NullString
+	for rows.Next() {
+		err = rows.Scan(&volumeID, &contentID, &text)
+		checkErr(err)
+
+		// trimmedText := strings.TrimSpace(text.String)
+		if text.Valid {
+			// fmt.Println(text.String)
+			trimmedText := strings.Join(strings.Fields(text.String), " ")
+			fmt.Println(trimmedText)
+			// fmt.Println(trimmedText)
+		}
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/apuchitnis/kobo-notes-exporter
 
 go 1.17
+
+require github.com/mattn/go-sqlite3 v1.14.9
